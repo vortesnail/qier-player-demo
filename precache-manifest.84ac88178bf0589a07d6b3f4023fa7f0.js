8b5b66f5f3bdcb939ee9f8ec67ec99cec3d5f801
vortesnail <1091331061@qq.com>
2019-10-23 19:14:08 +0800

    e..

---
+++ precache-manifest.84ac88178bf0589a07d6b3f4023fa7f0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b61bf65f20b45f3d52f41f5c3b2dcb9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "68a3d43fd53fad00ceec",
+    "url": "./static/css/main.668f23ff.chunk.css"
+  },
+  {
+    "revision": "f07e7115f3f269a304ec",
+    "url": "./static/js/2.3f2a9b9f.chunk.js"
+  },
+  {
+    "revision": "68a3d43fd53fad00ceec",
+    "url": "./static/js/main.830be5fa.chunk.js"
+  },
+  {
+    "revision": "b9f6e070e74448cd67c6",
+    "url": "./static/js/runtime-main.f88acd54.js"
+  },
+  {
+    "revision": "38ecd49b51074c82c24c8bb67405a354",
+    "url": "./static/media/erweima-1.38ecd49b.png"
+  },
+  {
+    "revision": "4d72d04328876b3dd0fd60b196147d76",
+    "url": "./static/media/erweima-2.4d72d043.png"
+  },
+  {
+    "revision": "4b4bff56dc6b337fd87901c1f161e532",
+    "url": "./static/media/iconfont.4b4bff56.ttf"
+  },
+  {
+    "revision": "726ca6988b5566164481e67e9cc6f800",
+    "url": "./static/media/iconfont.726ca698.woff"
+  },
+  {
+    "revision": "9d505f78f4481be9edb2fe3a58a237ee",
+    "url": "./static/media/iconfont.9d505f78.svg"
+  },
+  {
+    "revision": "aa38c1885849f0ca2017f1d4e541bc32",
+    "url": "./static/media/iconfont.aa38c188.eot"
+  },
+  {
+    "revision": "08419828a08bbb705e1e4fbab6a06e0f",
+    "url": "./static/media/qier-player-banner.08419828.png"
+  },
+  {
+    "revision": "d116ba094e5b3037d3429740fc3dbc53",
+    "url": "./static/media/video480p.d116ba09.mp4"
+  },
+  {
+    "revision": "fc81bd78c94963cc2203c5be083b2ee4",
+    "url": "./static/media/video720p.fc81bd78.mp4"
+  },
+  {
+    "revision": "fc81bd78c94963cc2203c5be083b2ee4",
+    "url": "./static/media/videoOrigin.fc81bd78.mp4"
+  }
+]);
